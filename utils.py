520ab43c8c7b26661624eeaa12546809f72e5f14
ppmdatix <paul.peseux@gmail.com>
2018-11-22 16:31:57 +0100

    best config

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 from matplotlib import pyplot as plt
 import numpy as np
 from keras.models import model_from_json
+import pandas as pd
+import os
+from os import system
+from keras.preprocessing import image
 
 
 ########
@@ -90,8 +94,104 @@ def unison_shuffled_copies(a, b):
 
 
 
-##############
-# PREDICTION #
-##############
+################
+# LOADING DATA #
+################
+def loadDATA(PATH,full=False, some=False, little=10):
+    #PATH = os.getcwd()
+    data_train_path_target = "data_airbus_defi/train/"
+    data_test_path = "data_airbus_defi/test/"
+
+    ###############
+    # SIZE LOADED #
+    ###############
+    if full:
+        training_size = float("inf")
+        testing_size = float("inf")
+    elif some:
+        training_size = 5000
+        testing_size = float("inf")
+    else:
+        training_size = little
+        testing_size = little
 
 
+
+    #########
+    # TRAIN #
+    #########
+
+    train_path = PATH +"/" + data_train_path_target + "target/"
+    train_data_target = os.listdir(train_path)
+    x_train = []
+    tdt = 0
+    for sample in (train_data_target):
+        img_path = train_path+sample
+        x = image.load_img(img_path)
+        x_train.append(np.array(x))
+        tdt += 1
+        if tdt > training_size:
+            break
+
+    train_path = PATH +"/" + data_train_path_target + "other/"
+    train_data_other = os.listdir(train_path)
+    x_train2 = []
+    tdo = 0
+    for sample in (train_data_other):
+        img_path = train_path+sample
+        x = image.load_img(img_path)
+        # preprocessing if required
+        x_train2.append(np.array(x))
+        tdo += 1
+        if tdo > training_size:
+            break
+
+
+    ########
+    # TEST #
+    ########
+
+    test_path = PATH+'/data_airbus_defi/test/'
+    test_data = os.listdir(test_path)
+    x_test = []
+    td = 0
+    output = pd.DataFrame(columns=["name"])
+    test_data = [str(x) + ".jpg" for x in range(len(test_data))]
+    for sample in (test_data):
+        output.append({"name": sample},ignore_index=True)
+        #print(sample)
+        img_path = test_path+sample
+        x = image.load_img(img_path)
+        # preprocessing if required
+        x_test.append(np.array(x))
+        td+=1
+        if td > testing_size:
+            break
+
+
+    # finally converting list into numpy array
+    x_train = np.array(x_train)
+    x_train2 = np.array(x_train2)
+    x_test = np.array(x_test) / 255.
+    XTRAIN = np.concatenate((x_train, x_train2), axis=0) / 255.
+    train_label1 = np.array([1] * x_train.shape[0] + [0] * x_train2.shape[0])
+    
+    return XTRAIN, train_label1, x_test
+
+
+def soumissionCSV(prediction, name):
+    test_path = PATH+'/data_airbus_defi/test/'
+    test_data = os.listdir(test_path)
+    X = pd.DataFrame()
+    X["name"] = test_data
+    X["prediction"] = prediction
+    X.set_index("name", inplace=True)
+    X.to_csv("soumissions/"+name + ".csv", sep=";")
+    print("CSV file written")
+
+def prediction_from_model(model,x_test):
+    pred = model.predict(x_test)
+    prediction = [cut_half(x) for x in pred]
+    print("some predictions")
+    print(prediction[:9])
+    return prediction
